89eca29463b93df467c82697a376c7a63ebf7ae1
KillerXtreme01 <77969044+KillerXtreme01@users.noreply.github.com>
2021-06-28 19:37:53 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -3,7 +3,10 @@ class Block{
       var options = {
           'restitution':0.8,
           'friction':1.0,
-          'density':1.0
+          'density':1.0,
+          'isStatic':false
+       
+        
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
